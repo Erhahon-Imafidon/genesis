08cb89ad7f9a98e1cb4834da0e286f1b8ae03249
Erhahon Imafidon <mafiosco4@gmail.com>
2024-09-21 21:44:30 +0100

    Implemented the form states using the formik library

--- app/login.tsx
+++ app/login.tsx
@@ -1,41 +1,47 @@
-import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, Image } from "react-native";
+import { Formik } from "formik";
 import AppTextInput from "@/components/ui/AppTextInput";
 import AppButton from "@/components/ui/AppButton";
 import Colors from "@/constants/Colors";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [pwd, setPwd] = useState("");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-        source={require("../assets/images/logo-red.png")}
-        style={styles.logo}
-      />
-      <AppTextInput
-        name="email"
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder={"Email"}
-        keyboardType={"email-address"}
-        textContentType={"emailAddress"}
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-      <AppTextInput
-        name="lock"
-        placeholder="password"
-        autoCapitalize={"none"}
-        autoCorrect={false}
-        secureTextEntry
-        textContentType={"password"}
-        onChangeText={(text) => setPwd(text)}
-        value={pwd}
-      />
-      <AppButton title={"Login"} onPress={() => console.log(email, pwd)} />
+      <Formik
+        initialValues={{ email: "", pwd: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit, values }) => (
+          <>
+            <Image
+              source={require("../assets/images/logo-red.png")}
+              style={styles.logo}
+            />
+            <AppTextInput
+              name="email"
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder={"Email"}
+              keyboardType={"email-address"}
+              textContentType={"emailAddress"}
+              onChangeText={handleChange("email")}
+              value={values.email}
+            />
+            <AppTextInput
+              name="lock"
+              placeholder="password"
+              autoCapitalize={"none"}
+              autoCorrect={false}
+              secureTextEntry
+              textContentType={"password"}
+              onChangeText={handleChange("pwd")}
+              value={values.pwd}
+            />
+            <AppButton title={"Login"} onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 };
