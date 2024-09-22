761774378cec7f27a41a29245c5145bc85f6a5f4
Erhahon Imafidon <mafiosco4@gmail.com>
2024-09-22 03:21:23 +0100

    Implemented a focused state

--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 import { StyleSheet, Image } from "react-native";
 import { Formik, FormikProps } from "formik";
 import * as Yup from "yup";
@@ -6,6 +7,8 @@ import * as Yup from "yup";
 import AppTextInput from "@/components/ui/AppTextInput";
 import AppButton from "@/components/ui/AppButton";
 import ErrorMessage from "@/components/ui/ErrorMessage";
+import AppText from "@/components/ui/AppText";
+import Colors from "@/constants/Colors";
 
 type FormValues = {
   email: string;
@@ -14,10 +17,19 @@ type FormValues = {
 
 const valdidationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
-  pwd: Yup.string().required().min(4).label("Password"),
+  pwd: Yup.string()
+    .required()
+    .min(6)
+    .matches(/[a-z]/, "must contain one lowercase letter")
+    .matches(/[A-Z]/, "must contain one uppercase letter")
+    .matches(/[0-9]/, "must contain one number")
+    .matches(/[!@#$%]/, "must contain one special character")
+    .min(8)
+    .label("Password"),
 });
 
 const LoginScreen = () => {
+  const [pwdFocus, setPwdFocus] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <Formik
@@ -60,9 +72,19 @@ const LoginScreen = () => {
               textContentType={"password"}
               onChangeText={handleChange("pwd")}
               value={values.pwd}
-              onBlur={() => setFieldTouched("pwd")}
+              onBlur={() => {
+                setFieldTouched("pwd");
+                setPwdFocus(false);
+              }}
+              onFocus={() => setPwdFocus(true)}
             />
             <ErrorMessage error={errors.pwd || ""} visible={touched.pwd} />
+            {pwdFocus && (
+              <AppText style={styles.pwdInfo}>
+                Password must Contain at least an uppercase, lowercase, a number
+                and one special character (!@#$%)
+              </AppText>
+            )}
 
             <AppButton title="Login" onPress={handleSubmit} />
           </>
@@ -84,6 +106,12 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     marginTop: 50,
     marginBottom: 20,
+  },
+
+  pwdInfo: {
+    fontSize: 12,
+    color: Colors.mediumGray,
+    marginBottom: 10,
   },
 });
 
