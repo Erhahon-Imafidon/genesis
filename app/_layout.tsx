965dcf2c07791418f5eb7d262057d5739daf62a5
Erhahon Imafidon <mafiosco4@gmail.com>
2024-12-17 20:17:47 +0100

    Setup the navigationColor theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,22 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from '@react-navigation/native';
+import NavigationTheme from '@/components/navigation/navigationTheme';
 
 const RootLayout = () => {
     return (
         <SafeAreaProvider>
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
-                <Stack.Screen name="(welcome)" />
-                <Stack.Screen name="(tabs)" />
-                <Stack.Screen name="+not-found" />
-            </Stack>
+            <ThemeProvider value={NavigationTheme}>
+                <Stack
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <Stack.Screen name="(welcome)" />
+                    <Stack.Screen name="(tabs)" />
+                    <Stack.Screen name="+not-found" />
+                </Stack>
+            </ThemeProvider>
         </SafeAreaProvider>
     );
 };
